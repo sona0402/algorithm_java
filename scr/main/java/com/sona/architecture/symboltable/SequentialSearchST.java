c0413843de1280575e885cbb76daeb4c394e144e
renfakai <renfakai@58.com>
2021-01-06 17:36:58 +0800

    feat:使用Linked做图

---
+++ scr/main/java/com/sona/architecture/symboltable/SequentialSearchST.java
@@ -0,0 +1,104 @@
+package com.sona.architecture.symboltable;
+
+import edu.princeton.cs.algs4.LinkedQueue;
+
+
+/**
+ * @author renfakai
+ * @version 1.0
+ * @since 2021/1/5
+ */
+public class SequentialSearchST<Key, Value> {
+
+    private int n;
+
+    private Node first;
+
+    //  内部数据结构
+    private class Node {
+        private final Key k;
+        private Value v;
+        private Node next;
+
+        public Node(Key k, Value v, Node next) {
+            this.k = k;
+            this.v = v;
+            this.next = next;
+        }
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean containsKey(Key k) {
+        if (null == k) throw new IllegalArgumentException("key must not null");
+        return get(k) != null;
+    }
+
+    public Value get(Key k) {
+        if (null == k) throw new IllegalArgumentException("key must not null");
+        //  first 可能为空
+        for (Node x = first; x != null; x = x.next) {
+            if (x.k == k) {
+                return x.v;
+            }
+        }
+        return null;
+    }
+
+    public void put(Key k, Value v) {
+        // 为什么我插入数据的时候老想着从后面插入呢，囧
+        if (null == k) throw new IllegalArgumentException("key must not null");
+        if (null == v) {
+            delete(k);
+            return;
+        }
+        // 如果已经存在数据
+        for (Node x = first; x != null; x = x.next) {
+            if (x.k.equals(k)) {
+                x.v = v;
+                return;
+            }
+        }
+        first = new Node(k, v, first);
+        n++;
+    }
+
+    public void delete(Key k) {
+        if (null == k) throw new IllegalArgumentException("key must not null");
+        first = delete(first, k);
+    }
+
+    private Node delete(Node x, Key k) {
+        if (x == null) return null;
+        if (k.equals(x.k)) {
+            n--;
+            return x.next;
+        }
+        x.next = delete(x.next, k);
+        return x;
+    }
+
+
+    public Iterable<Key> keys() {
+        LinkedQueue<Key> keysQ = new LinkedQueue<>();
+        for (Node x = first; x != null; x = x.next) {
+            keysQ.enqueue(x.k);
+        }
+        return keysQ;
+    }
+
+
+    public static void main(String[] args) {
+        SequentialSearchST<Integer, Integer> s = new SequentialSearchST<>();
+        for (int i = 0; i < 10; i++) {
+            s.put(i, i);
+        }
+        System.out.println(s.keys());
+    }
+}
---
+++ scr/main/java/com/sona/architecture/symboltable/package-info.java
@@ -0,0 +1,11 @@
+package com.sona.architecture.symboltable;
+/**
+ * symboltable = st
+ * {@see https://algs4.cs.princeton.edu/31elementary/}
+ *
+ * @version 1.0
+ * @author renfakai
+ * @since 2021/1/5
+ */
+
+
